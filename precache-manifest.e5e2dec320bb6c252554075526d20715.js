8c4c64bcb06d883e33e36779c3acb83371ba8b1a
pokorra <patrycja.pokora@gmail.com>
2020-11-04 13:27:24 +0100

    Updates

---
+++ precache-manifest.e5e2dec320bb6c252554075526d20715.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ea5d2c42d712a08cf11731b05c1f58",
+    "url": "/mazurscy/index.html"
+  },
+  {
+    "revision": "62e5196a6bb4f1c6d97b",
+    "url": "/mazurscy/static/css/main.1d4dfcc9.chunk.css"
+  },
+  {
+    "revision": "9ec1f009e4a62d47369a",
+    "url": "/mazurscy/static/js/2.442f0382.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/mazurscy/static/js/2.442f0382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e5196a6bb4f1c6d97b",
+    "url": "/mazurscy/static/js/main.372c6e8c.chunk.js"
+  },
+  {
+    "revision": "01711683c7efc1f6f5bc",
+    "url": "/mazurscy/static/js/runtime-main.133ee9ac.js"
+  },
+  {
+    "revision": "08ee46cfc630769991f317ae182c4ab2",
+    "url": "/mazurscy/static/media/bckg_large.08ee46cf.png"
+  },
+  {
+    "revision": "1ae6933949e44b633bf357bf8f328cf6",
+    "url": "/mazurscy/static/media/bckg_medium.1ae69339.png"
+  },
+  {
+    "revision": "eb011f64b207bd5a94d2fdce60787089",
+    "url": "/mazurscy/static/media/btm_left_l.eb011f64.png"
+  },
+  {
+    "revision": "f42cf410ffcad79f9d6502175285623a",
+    "url": "/mazurscy/static/media/btm_left_m.f42cf410.png"
+  },
+  {
+    "revision": "ae612d94cbdbf6b81791f8f6c5ad6d3b",
+    "url": "/mazurscy/static/media/btm_left_s.ae612d94.png"
+  },
+  {
+    "revision": "c39735d150ba0809db4dd503c5e165a6",
+    "url": "/mazurscy/static/media/btm_right_l.c39735d1.png"
+  },
+  {
+    "revision": "49f521981e04d94e90b3fb81a671e098",
+    "url": "/mazurscy/static/media/btm_right_m.49f52198.png"
+  },
+  {
+    "revision": "dc1629c60bb26ec7c752569678a2e978",
+    "url": "/mazurscy/static/media/btm_right_s.dc1629c6.png"
+  },
+  {
+    "revision": "8ccee9f389f06923eef099992f79999b",
+    "url": "/mazurscy/static/media/ftl_l.8ccee9f3.png"
+  },
+  {
+    "revision": "1f81b5732f628d11646cd2b5bc67a035",
+    "url": "/mazurscy/static/media/ftl_pic_l.1f81b573.png"
+  },
+  {
+    "revision": "734c5fcee333f043426a277360c27e4b",
+    "url": "/mazurscy/static/media/ftl_pic_m.734c5fce.png"
+  },
+  {
+    "revision": "85e798e887005b3aa867082f656e3aea",
+    "url": "/mazurscy/static/media/ftl_pic_s.85e798e8.png"
+  },
+  {
+    "revision": "e1fce1fe320da8cddd846995b7baeb77",
+    "url": "/mazurscy/static/media/ftw_pic_m.e1fce1fe.png"
+  },
+  {
+    "revision": "499ba89913f63d75978d6cf10f8ab78a",
+    "url": "/mazurscy/static/media/ftw_pic_s.499ba899.png"
+  },
+  {
+    "revision": "9c783264cbe6b0ca161561fc9d5ecebc",
+    "url": "/mazurscy/static/media/mazurscy_logo.9c783264.svg"
+  },
+  {
+    "revision": "464df4a3992b2172b73ad56c11e1be0f",
+    "url": "/mazurscy/static/media/top_left_l.464df4a3.png"
+  },
+  {
+    "revision": "2ef453031c4fc9609260b996cf31dddc",
+    "url": "/mazurscy/static/media/top_left_m.2ef45303.png"
+  },
+  {
+    "revision": "c4c6ecc5e0301a52b60588ef5ada22f2",
+    "url": "/mazurscy/static/media/top_left_s.c4c6ecc5.png"
+  },
+  {
+    "revision": "e64323afa332ea97e609aeda60d0e276",
+    "url": "/mazurscy/static/media/top_right_l.e64323af.png"
+  },
+  {
+    "revision": "43a03647c0115208ec0f501b98d3cdc9",
+    "url": "/mazurscy/static/media/top_right_m.43a03647.png"
+  },
+  {
+    "revision": "bfd9fb48f96991d9bde04a0a2c736ce1",
+    "url": "/mazurscy/static/media/top_right_s.bfd9fb48.png"
+  },
+  {
+    "revision": "3dc0b41033417e2787844f8614418281",
+    "url": "/mazurscy/static/media/wilga_logo.3dc0b410.svg"
+  }
+]);
