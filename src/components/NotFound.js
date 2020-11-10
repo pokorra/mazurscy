660fa00905046c46149fd7aea1992e4cab217ac7
pokorra <patrycja.pokora@gmail.com>
2020-11-10 13:21:21 +0100

    added missing notfound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import '../scss/notfind/notfind.scss';
 
 const NotFound =() => {
 
     return (
-        <div>nie ma tu niczego</div>
+        <div className='notfound'>
+            <h1 className='notfound-txt'> BŁĄD </h1>
+            <div className='notfound-inner'> </div>
+            <h2 classname='notfound-txt'>strona nie istnieje</h2>
+
+        </div>
     )
 }
 
